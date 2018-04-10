049b4ead874715d507da6df13dbbc4a417bfbbc8
Shawheen S <35010811+Shawheen93@users.noreply.github.com>
2018-04-09 22:41:57 -0700

    Create Student_Info.java

---
+++ Student_Info.java
@@ -0,0 +1,39 @@
+
+
+
+public class Student_Info {
+	int age;
+	boolean enroll;
+	String major;
+	String name;
+	//accessibility type-> name of the method
+	// First= Accessibility, Second=ReturnType, and Third=NameMethod
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public boolean isEnroll() {
+		return enroll;
+	}
+	public void setEnroll(boolean enroll) {
+		this.enroll = enroll;
+	}
+	public String getMajor() {
+		return major;
+	}
+	public void setMajor(String major) {
+		this.major = major;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+	
+}
